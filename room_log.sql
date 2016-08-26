1cb577912f22c7c5c02afaad7b2d995a689f7146
LimSeongJo <inventorlsj@gmail.com>
2016-08-26 22:21:03 +0900

    Delete room_log.sql

--- room_log.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists room_log;
-create table room_log (
-  id integer primary key autoincrement,
-  check_datetime string not null,
-  temperature integer not null,
-  humidity integer not null,
-  air_state integer not null
-);
